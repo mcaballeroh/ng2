8af1c90134cb2ad0d82686276a639befcf694fde
mcaballeroh <mcaballerohd@gmail.com>
2018-02-10 22:18:30 -0600

    class, ngClass, style, ngStyle, pipes, safe navigator operator. next: Smart and dumb components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
 interface Passenger {
   id: number,
   fullname: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  // this makes a property optional in case the api (or whatever) does not return it at all
+  // checkInDate?: number,
+  checkInDate: number | null,
+  children: Child[] | null
 }
 
 @Component({
@@ -22,9 +31,54 @@ interface Passenger {
 
       <ul *ngIf="passengers.length > 0">
         <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [class.checked-in]="passenger.checkedIn">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+          <div class="date">
+            Check in Date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMd' | uppercase) : 'Not checked in' }}
+          </div>
+          <div class="children">
+            <!-- passenger.children?.length: safe navigator operator, cool thing -->
+            Children: {{ passenger.children?.length || 0}}
+          </div>
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul *ngIf="passengers.length > 0">
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [ngClass]="{ 'checked-in' : passenger.checkedIn,
+            'checked-out' : !passenger.checkedIn
+          }">
+          </span>
           {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul *ngIf="passengers.length > 0">
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul *ngIf="passengers.length > 0">
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [ngStyle]="{
+            backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+          }">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+
     </div>
     `
   // templateUrl: './app.component.html'
@@ -35,27 +89,37 @@ export class AppComponent {
     {
       id: 1,
       fullname: 'Stephen',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null
     },
     {
       id: 2,
       fullname: 'Rose',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
     },
     {
       id: 3,
       fullname: 'James',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null
     },
     {
       id: 4,
       fullname: 'Mao',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: [{ name: 'Jessica', age: 1 }]
     },
     {
       id: 5,
       fullname: 'Vic',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null,
+      children: null
     }
   ];
 
